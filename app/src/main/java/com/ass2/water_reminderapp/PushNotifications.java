a3b50650a0f0edd0867a294e1f3150b2951b9eeb
usman182 <i190455@nu.edu.pk>
2022-12-06 19:15:55 +0500

    Push Notifications

---
+++ app/src/main/java/com/ass2/water_reminderapp/PushNotifications.java
@@ -0,0 +1,46 @@
+package com.ass2.water_reminderapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+public class PushNotifications extends AppCompatActivity {
+
+    NotificationManagerCompat notificationManagerCompat;
+    Notification notification;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_push_notifications);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("myCh", "My Channel", NotificationManager.IMPORTANCE_DEFAULT);
+
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "myCh")
+                .setSmallIcon(android.R.drawable.stat_notify_sync)
+                .setContentTitle("First Notification")
+                .setContentText("This is the Body of message");
+
+        notification = builder.build();
+
+        notificationManagerCompat = NotificationManagerCompat.from(this);
+    }
+
+    public void push(View view) {
+        notificationManagerCompat.notify(1, notification);
+    }
+}
